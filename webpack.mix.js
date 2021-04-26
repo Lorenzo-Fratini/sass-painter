d3ff8c8116460fbff73b3fe91e4812ea09f0bcf9
LorenzoF <lorenzo.fratini.4@gmail.com>
2021-04-26 17:34:32 +0200

    header update

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,9 +2,14 @@
 
 let mix = require('laravel-mix');
 
+
+
 mix
     .js('src/app.js', 'dist')
     .sass('src/style.scss', '')
+    .options({
+        processCssUrls: false
+    })
     .setPublicPath('dist')
     .copy('src/index.html', 'dist')
 ;
